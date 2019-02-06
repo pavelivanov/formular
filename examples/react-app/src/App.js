4067219ed8603851b2aa22a97ca436b7e9f8f9ba
Pavel Ivanov <grammka@gmail.com>
2019-02-07 00:51:46 +0300

    update to react 16.8

--- examples/react-app/src/App.js
+++ examples/react-app/src/App.js
@@ -1,34 +1,13 @@
-import React from 'react'
-// import { BrowserRouter as Router, Redirect, Route, NavLink } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 
 import './App.css'
 
-// import ShippingForm from './pages/ShippingForm'
 import PaymentPage from './pages/PaymentPage/PaymentPage'
 
-
-// const Nav = () => (
-//   <ul className="nav">
-//     <NavLink to="/shipping-form" activeClassName="active">Just shipping form</NavLink>
-//     <NavLink to="/payment-page" activeClassName="active">Payment page</NavLink>
-//   </ul>
-// )
-
-// const App = () => (
-//   <Router>
-//     <div className="wrapper">
-//       <Redirect to="/payment-page" />
-//       <Nav />
-//       <Route path="/shipping-form" component={ShippingForm} />
-//       <Route path="/payment-page" component={PaymentPage} />
-//     </div>
-//   </Router>
-// )
 
 const App = () => (
   <PaymentPage />
 )
-
 
 
 export default App
--- examples/react-app/src/pages/PaymentPage/PaymentPage.js
+++ examples/react-app/src/pages/PaymentPage/PaymentPage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment, Component, useReducer, useEffect } from 'react'
 import { FormxGroup } from 'formx'
 import { shipping, billing, creditCard } from '../../forms'
 
@@ -33,133 +33,120 @@ const getFormsGroup = (isSameAddress, paymentMethod) => {
   return new FormxGroup(forms)
 }
 
+let formGroup
 
-export default class PaymentPage extends Component {
 
-  constructor() {
-    super()
 
-    const sameAddress = false
-    const paymentMethod = 'creditCard'
-
-    this.state = {
-      sameAddress,
-      paymentMethod,
-    }
-
-    this.formGroup = getFormsGroup(sameAddress, paymentMethod)
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    const { sameAddress, paymentMethod } = this.state
-    const { sameAddress: newSameAddress, paymentMethod: newPaymentMethod } = nextState
-
-    if (sameAddress !== newSameAddress || paymentMethod !== newPaymentMethod) {
-      this.formGroup = getFormsGroup(newSameAddress, newPaymentMethod)
-    }
-  }
-
-  setInitialValues = () => {
-    this.formGroup.setValues({
-      shipping: {
-        firstName: 'foo',
-        lastName: 'bar',
-        street: '121 South Carondelete',
-        zipCode: '10095',
-        city: 'Los Angeles',
-        state: 'CA',
-      },
-      billing: {
-        firstName: 'foo',
-        lastName: 'bar',
-        street: '121 South Carondelete',
-        zipCode: '10095',
-        city: 'Los Angeles',
-        state: 'CA',
-      },
-      creditCard: {
-        cardNumber: '4242424242424242',
-        holderName: 'Foo Bar',
-        expDate: '1122',
-        cvc: '333',
-      },
-    })
-  }
-
-  clearFormsValues = () => {
-    this.formGroup.unsetValues()
-  }
-
-  clearCreditCardFields = () => {
-    this.formGroup.forms.creditCard.unsetValues()
-  }
-
-  handleChangePaymentMethod = (paymentMethod) => {
-    this.setState({
-      paymentMethod,
-    })
-  }
-
-  handleChangeSameAddress = (event) => {
-    this.setState({
-      sameAddress: event.target.checked,
-    })
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-
-    this.formGroup.submit()
-      .then((values) => {
-        console.log('values', values)
-      }, (errors) => {
-        console.error('errors', errors)
-        this.forceUpdate()
-      })
-  }
-
-  render() {
-    const { sameAddress, paymentMethod } = this.state
-
-    return (
-      <div className="content">
-        <div className="inlineItems">
-          <div>
-            <button type="button" onClick={this.setInitialValues}>Set initial values to all forms</button><br /><br />
-          </div>
-          <div>
-            <button type="button" onClick={this.clearFormsValues}>Clear forms values</button><br /><br />
-          </div>
-          <div>
-            <button type="button" onClick={this.clearCreditCardFields}>Clear credit card fields</button>
-          </div>
-        </div>
-        <form className="form" onSubmit={this.handleSubmit}>
-          <PaymentMethods onChange={this.handleChangePaymentMethod} />
-          <ShippingForm className="formSection" fields={shipping.fields} />
-          {
-            paymentMethod === 'creditCard' && (
-              <div className="formSection">
-                <label>
-                  <input type="checkbox" checked={sameAddress} onChange={this.handleChangeSameAddress} />
-                  Same address
-                </label>
-              </div>
-            )
-          }
-          {
-            this.formGroup.forms.billing && (
-              <BillingForm className="formSection" fields={billing.fields} />
-            )
-          }
-          {
-            this.formGroup.forms.creditCard && (
-              <CreditCardForm className="formSection" fields={creditCard.fields} />
-            )
-          }
-          <button className="submitButton" type="submit">Submit</button>
-        </form>
-      </div>
-    )
-  }
+const setInitialValues = () => {
+  formGroup.setValues({
+    shipping: {
+      firstName: 'foo',
+      lastName: 'bar',
+      street: '121 South Carondelete',
+      zipCode: '10095',
+      city: 'Los Angeles',
+      state: 'CA',
+    },
+    billing: {
+      firstName: 'foo',
+      lastName: 'bar',
+      street: '121 South Carondelete',
+      zipCode: '10095',
+      city: 'Los Angeles',
+      state: 'CA',
+    },
+    creditCard: {
+      cardNumber: '4242424242424242',
+      holderName: 'Foo Bar',
+      expDate: '1122',
+      cvc: '333',
+    },
+  })
 }
+
+const clearFormsValues = () => {
+  formGroup.unsetValues()
+}
+
+const clearCreditCardFields = () => {
+  formGroup.forms.creditCard.unsetValues()
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+
+  formGroup.submit()
+    .then((values) => {
+      console.log('values', values)
+    }, (errors) => {
+      console.error('errors', errors)
+    })
+}
+
+
+const PaymentPage = () => {
+  const [ state, setState ] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { isSameAddress: false, paymentMethod: 'creditCard' }
+  )
+
+  const { isSameAddress, paymentMethod } = state
+
+  formGroup = getFormsGroup(isSameAddress, paymentMethod)
+
+  const handleChangePaymentMethod = (paymentMethod) => {
+    setState({
+      paymentMethod,
+    })
+  }
+
+  const handleChangeSameAddress = (isSameAddress) => {
+    setState({
+      isSameAddress,
+    })
+  }
+
+  return (
+    <div className="content">
+      <div className="inlineItems">
+        <div>
+          <button type="button" onClick={setInitialValues}>Set initial values to all forms</button><br /><br />
+        </div>
+        <div>
+          <button type="button" onClick={clearFormsValues}>Clear forms values</button><br /><br />
+        </div>
+        <div>
+          <button type="button" onClick={clearCreditCardFields}>Clear credit card fields</button>
+        </div>
+      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <PaymentMethods onChange={handleChangePaymentMethod} />
+        <ShippingForm className="formSection" fields={shipping.fields} />
+        {
+          paymentMethod === 'creditCard' && (
+            <div className="formSection">
+              <label>
+                <input type="checkbox" checked={isSameAddress} onChange={handleChangeSameAddress} />
+                Same address
+              </label>
+            </div>
+          )
+        }
+        {
+          formGroup.forms.billing && (
+            <BillingForm className="formSection" fields={billing.fields} />
+          )
+        }
+        {
+          formGroup.forms.creditCard && (
+            <CreditCardForm className="formSection" fields={creditCard.fields} />
+          )
+        }
+        <button className="submitButton" type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+
+export default PaymentPage
