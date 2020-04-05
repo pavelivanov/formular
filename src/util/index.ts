2a06cd04b7407ada9e2a54c36bde979e171658b6
Pavel Ivanov <grammka@gmail.com>
2020-04-05 17:49:10 +0300

    - remove deprecated Field "set" event - replace Form "update" event with "force update" - change update() method to forceUpdate() - remove unmet tags/*

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,12 +1,10 @@
 import CancelablePromise from './CancelablePromise'
-import asyncEvery from './asyncEvery'
 import asyncSome from './asyncSome'
 import debounce from './debounce'
 
 
 export {
   CancelablePromise,
-  asyncEvery,
   asyncSome,
   debounce,
 }
